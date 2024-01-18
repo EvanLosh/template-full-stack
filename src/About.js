fe9e275fa51ad270717cc0468aa7935ebd02d255
Evan Losh <evand.losh@gmail.com>
2024-01-18 09:32:46 -0500

    updated about page

--- src/About.js
+++ src/About.js
@@ -7,13 +7,22 @@ function About() {
             <h2>What is this app for?</h2>
             <p>This web app is a template for starting new full-stack projects using React, Flask, and SQLalchemy.</p>
             <h2>On the backend</h2>
-            <p>models.py defines two model classes (people and homes) and an object relationship model. Every person has one home, and Each home may have many people. models.py also defines serialization rules and validates data. Each class has a corresponding table in a SQL database, and each instance is synced to a row of data.</p>
-            <p>app.py defines API endpoints and server responses.</p>
-            <p>To set up the database, ...</p>
-            <p>To run the server, ...</p>
+            <p><code>models.py</code> defines two model classes (people and homes) and an ORM. Every person has one home, and Each home may have many people. <code>models.py</code> also defines serialization rules and validates data. Each class has a corresponding table in a SQL database, and each instance is synced to a row of data.</p>
+            <p><code>app.py</code> defines API endpoints and server responses.</p>
+            <p>To set up the database, run these commands in the project directory:</p>
+            <p class="code"># pipenv install</p>
+            <p class="code"># pipenv shell</p>
+            <p>And then in the server directory:</p>
+            <p class="code"># flask db init</p>
+            <p class="code"># flask db migrate -m "initial migration"</p>
+            <p class="code"># flask db upgrade head</p>
+            <p>To populate the database with fake data, run <code> seed.py</code>.</p>
+            <p>To start the server, run <code> app.py</code>.</p>
             <h2>On the frontend</h2>
-            <p>App.js fetches data from the server when it mounts. It also features a React Router. The user can perform CRUD actions on the server database via form submission.</p>
-            <p>To start the frontend, ...</p>
+            <p><code>App.js</code> fetches data from the server when it mounts. It also features a React Router. The user can perform CRUD actions on the server database via form submission.</p>
+            <p>To start the frontend, run these commands in the project directory:</p>
+            <p class="code"># npm install</p>
+            <p class="code"># npm run server</p>
         </div>
     );
 }
